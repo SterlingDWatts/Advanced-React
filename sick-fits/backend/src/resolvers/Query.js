8c5caa2b96185d4c30835f5341593efccb53ceb0
Sterling Watts <SterlingDWatts@gmail.com>
2020-07-28 16:40:41 -0700

    Create OrdersPage

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -45,6 +45,19 @@ const Query = {
 
     return order;
   },
+  async orders(parent, args, ctx, info) {
+    const userId = ctx.request.userId;
+    if (!userId) throw new Error("You must be logged in!");
+
+    return ctx.db.query.orders(
+      {
+        where: {
+          user: { id: userId },
+        },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Query;
